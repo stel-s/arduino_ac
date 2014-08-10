a458767ad987ea9f0accd9d633cc695b5f5caa44
ssp13 <ssp13@users.noreply.github.com>
2014-08-10 17:53:52 +0300

    "s'

---
+++ ir_ac_pusher/pusher_ir_ac/pusher_ir_ac.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <avr/pgmspace.h>
+#include <PusherClient.h>
+#include <IRremote.h>
+
+IRsend irsend;
+unsigned int power[60]={8400,4200,550,1600,550,550,550,550,550,550,500,1600,550,600,500,550,550,550,550,550,500,550,550,550,550,550,500,600,500,550,550,550,550,550,500,550,550,1600,600,1550,600,1600,550,550,550,550,500,1600,600,550,500,1600,600,550,500,550,550,1600,550};
+void setup() 
+{
+  
+  Serial.begin(9600);
+ pinMode(8,OUTPUT);
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+  if (Ethernet.begin(mac) == 0)
+  {
+    for(;;);
+  }
+  
+  if (!Pusher.connect())
+  {
+    Serial.println("not connected");
+    for(;;);
+  } 
+  Pusher.bind("client-turnon", turnon);
+  Pusher.bind("client-turnoff", turnoff);
+  Pusher.subscribePrivate("private-channel");
+  //Pusher.triggerPrivateEvent("private-ChannelName", "client-eventName", "\"\"");
+ //Pusher.triggerPrivateEvent("private-ChannelName", "client-eventName", "\"1\"");
+
+}
+
+  
+ void turnon(const String& name,const String& data){
+     digitalWrite(8,HIGH);
+     for(int i=1;i<3;i++){
+      irsend.sendRaw(power,60,38); 
+      delay(402);
+     }
+  }
+  void turnoff(const String& name,const String& data){
+     digitalWrite(8,LOW);
+
+     for(int i=1;i<3;i++){
+          //irsend.sendRaw(display,60,38); 
+          
+     }
+  }
+void loop() 
+  {   
+  Pusher.monitor();  
+ int lasttime;
+
+  unsigned long time = millis();  
+  if (time > lasttime + 1000)
+  {
+    //signalActualState();
+    lasttime = time;
+  }
+}
+
