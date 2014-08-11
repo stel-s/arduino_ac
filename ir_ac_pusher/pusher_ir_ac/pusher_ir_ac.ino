eab409c87622956a8cfdc6ffb0b804baf812c84f
ssp13 <ssp13@users.noreply.github.com>
2014-08-11 14:07:22 +0300

    "updates"

--- ir_ac_pusher/pusher_ir_ac/pusher_ir_ac.ino
+++ ir_ac_pusher/pusher_ir_ac/pusher_ir_ac.ino
@@ -5,7 +5,9 @@
 #include <IRremote.h>
 
 IRsend irsend;
-unsigned int power[60]={8400,4200,550,1600,550,550,550,550,550,550,500,1600,550,600,500,550,550,550,550,550,500,550,550,550,550,550,500,600,500,550,550,550,550,550,500,550,550,1600,600,1550,600,1600,550,550,550,550,500,1600,600,550,500,1600,600,550,500,550,550,1600,550};
+unsigned int powerOn[60]={8400,4200,550,1600,550,550,550,550,550,550,500,1600,550,600,500,550,550,550,550,550,500,550,550,550,550,550,500,600,500,550,550,550,550,550,500,550,550,1600,600,1550,600,1600,550,550,550,550,500,1600,600,550,500,1600,600,550,500,550,550,1600,550};
+unsigned int powerOff[60]={8400,4200,550,1600,550,550,550,500,600,500,550,1600,600,500,550,550,550,500,600,1600,550,1600,550,550,550,500,600,500,550,550,550,550,550,500,600,500,550,550,550,500,600,500,550,550,550,1600,550,550,550,1600,550,550,550,550,550,500,550,1600,600};
+
 void setup() 
 {
   
@@ -16,12 +18,14 @@ void setup()
   {
     for(;;);
   }
-  
-  if (!Pusher.connect())
-  {
+  //TODO try connecting until success
+  while(!Pusher.connect()){
     Serial.println("not connected");
-    for(;;);
-  } 
+ 
+  delay(2000);
+   Serial.println("reconnectin");
+  }
+ 
   Pusher.bind("client-turnon", turnon);
   Pusher.bind("client-turnoff", turnoff);
   Pusher.subscribePrivate("private-channel");
@@ -33,29 +37,32 @@ void setup()
   
  void turnon(const String& name,const String& data){
      digitalWrite(8,HIGH);
-     for(int i=1;i<3;i++){
-      irsend.sendRaw(power,60,38); 
+     for(int i=0;i<2;i++){
+      irsend.sendRaw(powerOn,60,38); 
       delay(402);
      }
   }
   void turnoff(const String& name,const String& data){
      digitalWrite(8,LOW);
 
-     for(int i=1;i<3;i++){
-          //irsend.sendRaw(display,60,38); 
+     for(int i=0;i<2;i++){
+          irsend.sendRaw(powerOff,60,38); 
           
      }
   }
 void loop() 
   {   
-  Pusher.monitor();  
- int lasttime;
+  //TODO checkConnected();
+  //if false reconnect();
+  
+    Pusher.monitor();  
+   int lasttime;
 
   unsigned long time = millis();  
-  if (time > lasttime + 1000)
-  {
+    if (time > lasttime + 1000)
+    {
     //signalActualState();
     lasttime = time;
-  }
+    }
 }
 
